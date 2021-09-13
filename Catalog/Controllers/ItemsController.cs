68283c9e2765b6e578b4cffb8146877615fd4226
claude cavour <aristide22lapo@gmail.com>
2021-09-13 16:32:19 +0200

    my first commit

---
+++ Catalog/Controllers/ItemsController.cs
@@ -0,0 +1,88 @@
+﻿using Catalog.Dtos;
+using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private readonly IItemsRepository _repository;
+
+        public ItemsController(IItemsRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ItemDto>> GetItemsAsync()
+        {
+            var items = (await _repository.GetItemsAsync()).Select(item => item.AsDto());
+
+            return items;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ItemDto>> GetItemAsync(Guid id)
+        {
+            var item =await _repository.GetItemAsync(id);
+
+            if (item == null)
+                return NotFound();
+
+            return item.AsDto();
+        }
+        [HttpPost]
+        public async Task<ActionResult<ItemDto>> CreateItemAsync(CreateItemDto itemDto)
+        {
+            Item item = new Item()
+            {
+                Id = Guid.NewGuid(),
+                Name = itemDto.Name,
+                Price = itemDto.Price,
+                CreatedDate = DateTimeOffset.UtcNow
+            };
+            await _repository.CreateItemsAsync(item);
+
+            return CreatedAtAction(nameof(GetItemAsync), new { id = item.Id }, item.AsDto());
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateItemAsync(Guid id, UpdateItemDto itemDto)
+        {
+            var existingItem =await _repository.GetItemAsync(id);
+            if (existingItem == null)
+            {
+                return NotFound();
+            }
+            Item updatedItem = existingItem with
+            {
+                Name = itemDto.Name,
+                Price = itemDto.Price
+            };
+            await _repository.UpdateItemAsync(updatedItem);
+
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public ActionResult DeleteItem(Guid id)
+        {
+            var existingItem = _repository.GetItemAsync(id);
+            if(existingItem == null)
+            {
+                return NotFound();
+            }
+            _repository.DeleteItemAsync(id);
+            return NoContent();
+        }
+    }
+
+    
+}
---
+++ Catalog/Repositories/IItemsRepository.cs
@@ -0,0 +1,16 @@
+﻿using Catalog.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.Repositories
+{
+    public interface IItemsRepository
+    {
+        Task<Item> GetItemAsync(Guid id);
+        Task<IEnumerable<Item>> GetItemsAsync();
+        Task CreateItemsAsync(Item item);
+        Task UpdateItemAsync(Item item);
+        Task DeleteItemAsync(Guid id);
+    }
+}
